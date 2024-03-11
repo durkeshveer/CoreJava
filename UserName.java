5c814ad4d4a4270a2c25f3f46cb527d60c0801b7
durkeshveer <162946489+durkeshveer@users.noreply.github.com>
2024-03-11 16:31:57 +0530

    nextline

---
+++ UserName.java
@@ -0,0 +1,14 @@
+package Day3;
+import java.util.*;
+public class UserName {
+
+	public static void main(String[] args) {
+	 Scanner input = new Scanner(System.in );
+	 System.out.println("Enter your name :");
+	 String n= input.nextLine();
+	 System.out.println("hello " +n+ "");
+	 
+
+	}
+
+}
